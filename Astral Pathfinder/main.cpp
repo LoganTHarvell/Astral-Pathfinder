aa8eb1697377a57fade20d03397ae37aadd058a6
LoganTHarvell <logan@harvell.us>
2018-01-16 01:10:43 -0500

    Initial Commit

---
+++ Astral Pathfinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Astral Pathfinder
+//
+//  Created by Logan Harvell on 1/16/18.
+//  Copyright © 2018 Logan Harvell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
