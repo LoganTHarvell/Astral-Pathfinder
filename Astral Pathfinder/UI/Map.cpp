aaff2574f16587bac0a123c98a3d767ec8b17796
LoganTHarvell <logan@harvell.us>
2018-02-14 05:03:44 -0500

    removed obsolete code

--- Astral Pathfinder/UI/Map.cpp
+++ Astral Pathfinder/UI/Map.cpp
@@ -20,33 +20,3 @@ SDL_Point Map::uiPosition(SDL_Point position) {
   
   return { x, y };
 }
-
-//void Map::loadShip() {
-//  // Initialize Ship
-//  SDL_Rect tmpRect = { 0, 0, 24, 45 };
-//
-//  int xpos = planets[0] -> getPosition().x;
-//  int ypos = planets[0] -> getPosition().y;
-//  ship = new GameObject("Resources/Assets/simpleSpaceship.png",
-//                        tmpRect, xpos, ypos);
-//  ship->setPosition(xpos - (ship->getSize().w/2), ypos - (ship->getSize().h/2));
-//}
-
-//void Map::update() {
-//  // Updates planet game objects
-//  for (int i = 0; i < NUMBER_OF_PLANETS; i++)
-//      planets[i]->update();
-//  
-//  // Updates the ship game object
-//  ship->update();
-//}
-//
-//void Map::render() {
-//  // Renders the planet game objects
-//  for (int i = 0; i < NUMBER_OF_PLANETS; i++)
-//      planets[i]->render();
-//  
-//  // Renders the ship game object
-//  ship->render();
-//}
-
