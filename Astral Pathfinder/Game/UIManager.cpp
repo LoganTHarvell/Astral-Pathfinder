df7747201cb8081cff1800ce0f426b89cdb8fc35
LoganTHarvell <logan@harvell.us>
2018-02-21 16:49:09 -0500

    created UIManager class

---
+++ Astral Pathfinder/Game/UIManager.cpp
@@ -0,0 +1,9 @@
+//
+//  UIManager.cpp
+//  Astral Pathfinder
+//
+//  Created by Logan Harvell on 2/21/18.
+//  Copyright © 2018 Logan Harvell, Ian Holdeman. All rights reserved.
+//
+
+#include "UIManager.hpp"
---
+++ Astral Pathfinder/Game/UIManager.hpp
@@ -0,0 +1,26 @@
+//
+//  UIManager.hpp
+//  Astral Pathfinder
+//
+//  Created by Logan Harvell on 2/21/18.
+//  Copyright © 2018 Logan Harvell, Ian Holdeman. All rights reserved.
+//
+
+#ifndef UIManager_hpp
+#define UIManager_hpp
+
+
+class UIManager {
+  
+public:
+  void init();
+  
+  void update();
+  void render();
+  
+private:
+  
+  
+};
+
+#endif /* UIManager_hpp */
