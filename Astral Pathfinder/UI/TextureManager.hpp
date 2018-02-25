fc14e459a8bc0f90470ab6f2eb3ea950d02bdcd6
iholdeman1 <ian.holdeman0510@gmail.com>
2018-02-24 22:32:50 -0500

    removed commented texture function

--- Astral Pathfinder/UI/TextureManager.hpp
+++ Astral Pathfinder/UI/TextureManager.hpp
@@ -20,7 +20,6 @@ public:
   // MARK: - TextureManager Methods
   // Loads textures from image file
   static SDL_Texture *loadTexture(const char *textureFile);
-  static SDL_Texture *loadTextTexture(_TTF_Font font, const char *text, SDL_Color color);
   
 };
 
